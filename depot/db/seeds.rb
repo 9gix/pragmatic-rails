8453f830dc0c539f8af5f054a83cbc101f0e0756
Eugene <yeo.eugene.oey@gmail.com>
2013-12-12 12:44:46 +0800

    Seed Data

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create(
+    :title => 
+        'Clean Code: A Handbook of Agile Software Craftsmanship',
+    :description => 
+        %{Even bad code can function. But if code isnt clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesnt have to be that way.
+         Noted software expert Robert C. Martin presents a revolutionary paradigm with Clean Code: A Handbook of Agile Software Craftsmanship . Martin has teamed up with his colleagues from Object Mentor to distill their best agile practice of cleaning code "on the fly" into a book that will instill within you the values of a software craftsman and make you a better programmer-but only if you work at it.},
+    :image_url => '/images/clean_code.jpg',
+    :price => 49.50)
+
+Product.create(
+    :title => 'Secrets of the JavaScript Ninja',
+    :description => 
+        %{Secrets of the Javascript Ninja takes you on a journey towards mastering modern JavaScript development in three phases: design, construction, and maintenance. Written for JavaScript developers with intermediate-level skills, this book will give you the knowledge you need to create a cross-browser JavaScript library from the ground up.},
+    :image_url => '/images/ninja.jpg',
+    :price => 40.25)
