7a0c40201bba242ebb08394e732bad0bf20e558d
Eugene <yeo.eugene.oey@gmail.com>
2013-12-13 22:15:55 +0800

    Fix typo bug

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_then_or_equal_to => 0.01}
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {
     :with => %r{\.(jpg|gif|png)$}i,
