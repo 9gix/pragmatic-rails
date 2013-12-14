0150c38fbc187326dc4a31a0ddb31098c3fff451
Eugene <yeo.eugene.oey@gmail.com>
2013-12-14 19:15:14 +0800

    Add unit test title must be unique

--- depot/test/unit/product_test.rb
+++ depot/test/unit/product_test.rb
@@ -52,4 +52,14 @@ class ProductTest < ActiveSupport::TestCase
       assert new_product(name).invalid? "#{name} shouldn't be valid" 
     end
   end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(
+	    :title => products(:javascript).title,
+	    :description => "hello",
+	    :image_url => "js.png",
+	    :price => 34.90)
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+  end
 end
