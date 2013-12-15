a9c680522e7cb5b33ea25a75a5fc28284b78a872
Eugene <yeo.eugene.oey@gmail.com>
2013-12-15 16:58:00 +0800

    Add to cart controller

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -40,7 +40,9 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(params[:line_item])
+    @cart = current_cart
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(:product_id => product.id)
 
     respond_to do |format|
       if @line_item.save
