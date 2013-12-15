d6dcff681b603ecbe2d53cbcb32211d62b558740
Eugene <yeo.eugene.oey@gmail.com>
2013-12-15 16:23:09 +0800

    Current Cart private method

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
   def time
     Time.now
   end
+
+  private
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
 end
