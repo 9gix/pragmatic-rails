7ceaa6aaf9a42e22c764f96017fd8a2d33a915be
Eugene <yeo.eugene.oey@gmail.com>
2013-12-15 22:33:51 +0800

    Visit Counter

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
   def index
     @products = Product.all
+    @counter = increment_counter
   end
 end
