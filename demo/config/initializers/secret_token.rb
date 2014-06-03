a1ead633c482f79eb8e7ed58fb445dafabc6b593
Eugene <yeo.eugene.oey@gmail.com>
2014-06-03 10:58:30 +0800

    Remove Demo project

--- demo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Demo::Application.config.secret_token = 'b917e28e24fa5b02498a76898030cfbaadc5544ec4bb8e308818cb25b9083e5af9623e052854275d4da675ea2042ba5c5fa9e1f7da32b67d9240376094ac0ba8'
